83935d6d706942d2e133a3fa74815a525e3da4cc
Sahilnarang310 <softwaredev310@gamil.com>
2023-06-25 18:30:37 +0530

    Created singup backend api's

---
+++ Task-3/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const routes = require('./routes/authRoutes');
+const path = require('path');
+const authRoutes = require('./routes/authRoutes');
+// const expenseRoutes = require('./routes/expensesRoutes');
+const sequelize = require('./util/database');
+
+const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors());
+
+app.use(bodyParser.json({ extended: false }));
+
+app.use(routes);
+app.use(authRoutes);
+// app.use(expenseRoutes);
+
+sequelize
+    .sync()
+    .then(() => {
+        app.listen(3000)
+    })
+    .catch(err => console.error(err));
